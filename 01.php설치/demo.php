e1154224be45e9fb423320b71494f859b840c59a
khjpink <122282659+khjpink@users.noreply.github.com>
2025-02-14 18:08:24 +0900

    Add files via upload

---
+++ 01.php설치/demo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+</head>
+<body>
+<h2>php 작업환경 준비</h2>
+<?php
+/*
+XAMPP 프로그램 설치 
+	잼프는 Apache, PHP해석기, MySQL 프로그램을 포함한다 
+	https://www.apachefriends.org/
+
+
+XAMPP 프로그램 동작 확인
+	설치된 XAMPP Control Panel 의 Apache 를 Start 클릭
+	에러가 없다면 PID, Port가 나오고 running이 뜬다 
+	브라우저에 다음과 같이 입력 
+	http://localhost/  
+	제대로 설치 됐다면 XAMPP 메인 화면이 뜬다 (Apache가 구동되고 있어야 한다)	
+
+비주얼 스튜디오 코드 설치
+	https://code.visualstudio.com/	
+	설치가 완료되면 확장 아이콘 클릭 후 korean 한국어 확장팩 을 설치
+
+작업 폴더 준비
+	C:\xampp\htdocs 폴더에 source폴더를 생성한다. 
+	php파일을 브라우저에 실행하려면 다음과 같이 입력
+	http://localhost/source/01.php%EC%84%A4%EC%B9%98/demo.php 
+
+*/
+?> 
+</h1>
+</body>
+</html>
